b5838226d6b06e6f36e5199c0c26c9b797f1d136
Jake Smith <jakehockey10@gmail.com>
2016-07-24 15:13:57 -0600

    assets I want to provide for developer using gem

--- Gemfile
+++ Gemfile
@@ -2,3 +2,12 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in popcircle.gemspec
 gemspec
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platform: :mri
+end
+
+group :development do
+  gem 'pg'
+end
--- app/helpers/popcircle/rails/popcircle_helper.rb
+++ app/helpers/popcircle/rails/popcircle_helper.rb
@@ -1,30 +1,48 @@
-module PopcircleHelper
-  include ActionView::Helpers::TagHelper
+module Popcircle
+  module Rails
+    module PopcircleHelper
+      include ActionView::Helpers::TagHelper
+      include FontAwesome::Rails::IconHelper
 
-  def popcircle(*circles)
-    options        = circles.extract_options!
-    options[:list] ||= 'pops'
-    tag.div class: 'popcircle-box' do
-      tag.div class: 'popcircle-trigger'
-      tag.div class: 'popcircle' do
-        tag.ul id: options[:list] do
-          circles.each do |circle|
-            tag.li do
-              link_to (circle[:link] ||= '') do
-                if circle[:image]
-                  image_tag circle[:image]
-                elsif circle[:icon]
-                  fa_icon circle[:icon]
-                end
-              end
+      def popcircle(*circles)
+        options        = circles.length > 1 ? circles.extract_options! : {}
+        options[:list] ||= 'pops'
+        content_tag :div, class: 'popcircle-box' do
+          trigger + list(circles, options)
+        end if circles
+      end
+
+      private
+
+      def trigger
+        content_tag :div, '', class: 'popcircle-trigger'
+      end
+
+      def list(circles, options)
+        content_tag :div, class: 'popcircle' do
+          content_tag :ul, id: options[:list] do
+            circles.each do |circle|
+              concat list_item(circle)
             end
           end
         end
       end
-    end if circles
-  end
-end
 
-ActiveSupport.on_load(:action_view) do
-  include PopcircleHelper
+      def list_item(circle)
+        content_tag :li, link_with_image_or_icon(circle).html_safe
+      end
+
+      def link_with_image_or_icon(circle)
+        link_to (circle[:link] ||= '') do
+          if circle[:image]
+            image_tag circle[:image]
+          elsif circle[:icon]
+            fa_icon circle[:icon]
+          else
+            'UH OH'
+          end
+        end
+      end
+    end
+  end
 end
