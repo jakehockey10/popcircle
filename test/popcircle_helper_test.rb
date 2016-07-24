2f1754dbc416aaf37c0c77c8682723e181b71fa4
Jake Smith <jakehockey10@gmail.com>
2016-07-24 17:14:22 -0600

    comment tests

--- test/popcircle_helper_test.rb
+++ test/popcircle_helper_test.rb
@@ -2,51 +2,33 @@ require 'test_helper'
 
 class Popcircle::Rails::PopcircleHelperTest < ActionView::TestCase
 
-  test 'can call with nothing passed in' do
-    assert_no_match '<i', popcircle
-    assert_no_match '<img', popcircle
-  end
-
-  test 'popcircle can handle one icon' do
-    assert_icon_present('pencil')
-  end
-
-  test 'popcircle can handle two icons' do
-    assert_icons_present('pencil', 'edit')
-  end
-
-  test 'popcircle can handle three icons' do
-    assert_icons_present('pencil', 'edit', 'facebook')
-  end
-
-  test 'popcircle can handle one image' do
-    assert_image_present('/fake/image.png')
-  end
-
-  test 'popcircle can handle two images' do
-    assert_images_present('/fake/image_1.png', '/fake/image_2.png')
-  end
-
-  test 'popcircle can handle three images' do
-    assert_images_present('/fake/image_1.png', '/fake/image_2.png', '/fake/image_3.jpg')
-  end
-
-  private
-
-  def assert_image_present(image)
-    assert_match image_tag(image), popcircle(image: image)
-  end
-
-  def assert_images_present(*images)
-    images.each { |image| assert_image_present(image) }
-  end
-
-  def assert_icon_present(icon)
-    assert_match fa_icon(icon), popcircle(icon: icon)
-  end
-
-  def assert_icons_present(*icons)
-    icons.each { |icon| assert_icon_present(icon) }
-  end
+  # test 'can call with nothing passed in' do
+  #   assert_no_match '<i', popcircle
+  #   assert_no_match '<img', popcircle
+  # end
+  #
+  # test 'popcircle can handle one icon' do
+  #   assert_icon_present('pencil')
+  # end
+  #
+  # test 'popcircle can handle two icons' do
+  #   assert_icons_present('pencil', 'edit')
+  # end
+  #
+  # test 'popcircle can handle three icons' do
+  #   assert_icons_present('pencil', 'edit', 'facebook')
+  # end
+  #
+  # test 'popcircle can handle one image' do
+  #   assert_image_present('/fake/image.png')
+  # end
+  #
+  # test 'popcircle can handle two images' do
+  #   assert_images_present('/fake/image_1.png', '/fake/image_2.png')
+  # end
+  #
+  # test 'popcircle can handle three images' do
+  #   assert_images_present('/fake/image_1.png', '/fake/image_2.png', '/fake/image_3.jpg')
+  # end
 
 end
