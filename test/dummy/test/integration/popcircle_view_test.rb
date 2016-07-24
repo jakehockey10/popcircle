a3f6d1446ce7fb912b6490ffcdc74998be674f41
Jake Smith <jakehockey10@gmail.com>
2016-07-24 17:13:34 -0600

    comment tests for now; TODO get tests to work

--- test/dummy/test/integration/popcircle_view_test.rb
+++ test/dummy/test/integration/popcircle_view_test.rb
@@ -7,6 +7,6 @@ class PopcircleViewTest < ActionDispatch::IntegrationTest
     assert_select '.popcircle-trigger', count: 1
     assert_select '.popcircle', count: 1
     assert_select 'ul#pops', count: 1
-    assert_select 'ul#pops > li > a[href=""] > i.fa.fa-pencil'
+    assert_select 'ul#pops > li > a[href=""] > img', count: 5
   end
 end
